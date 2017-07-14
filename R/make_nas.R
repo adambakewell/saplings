08ebc48a8ed539bd63a38632d6077e2383ef5a2f
adambakewell <ab1348@york.ac.uk>
2017-07-14 16:06:38 +0100

    make_nas script update

--- R/make_nas.R
+++ R/make_nas.R
@@ -1,12 +1,20 @@
-make_nas<-function(data, number){
-	new.data<-data
-	no.na<-which(!is.na(data))
-	indexes<-matrix(data=c(1:(length(data)*nrow(data))), nrow=nrow(data))
-	to.change<-sample(no.na, number)
+make_nas<-function(data, number, keep_n=2){	
+	new.data<-data	
+	no.na<-which(!is.na(data))	
+	indexes<-matrix(data=c(1:(length(data)*nrow(data))), nrow=nrow(data))	
+	keepers<-character()	
+	for(j in 1:ncol(data)){		
+			search<-no.na[no.na %in% indexes[,j]]
+			keep<-sample(search, 2)
+			keepers<-append(keepers, keep)
+	}
+	to.change<-sample(no.na[! no.na %in% keepers], number)
 	to.change.ind<-lapply(to.change, function(x) {which(indexes==x, arr.ind=TRUE)})
 	to.change.ind<-matrix(unlist(to.change.ind), ncol=2, byrow=TRUE)
 	for(i in 1:nrow(to.change.ind)){
-		new.data[to.change.ind[i,1],to.change.ind[i,2]]<-NA
+		rownum<-to.change.ind[i,1]
+		colnum<-to.change.ind[i,2]
+		new.data[rownum, colnum]<-NA
 	}
 	return(new.data)
 }
